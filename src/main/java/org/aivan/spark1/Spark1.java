182be46b74234b676d5836b3c82033926440fa20
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2016-12-02 23:54:23 +0100

    Add CORS

--- src/main/java/org/aivan/spark1/Spark1.java
+++ src/main/java/org/aivan/spark1/Spark1.java
@@ -5,9 +5,44 @@ import static org.aivan.spark1.util.JsonUtil.*;
 
 public class Spark1 {
 	public static void main(String[] args) {
+
+		
+
 		staticFiles.location("/public");
 		get("/hello", (req, res) -> dataToJson(new Hello("ola")));
+		
+		enableCORS("*", "*", "*");
+		
 	}
+
+	// Enables CORS on requests. This method is an initialization method and
+	// should be called once.
+	private static void enableCORS(final String origin, final String methods, final String headers) {
+
+		options("/*", (request, response) -> {
+
+			String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+			if (accessControlRequestHeaders != null) {
+				response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+			}
+
+			String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+			if (accessControlRequestMethod != null) {
+				response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+			}
+
+			return "OK";
+		});
+
+		before((request, response) -> {
+			response.header("Access-Control-Allow-Origin", origin);
+			response.header("Access-Control-Request-Method", methods);
+			response.header("Access-Control-Allow-Headers", headers);
+			// Note: this may or may not be necessary in your particular application
+			response.type("application/json");
+		});
+	}
+
 }
 
 class Hello {
